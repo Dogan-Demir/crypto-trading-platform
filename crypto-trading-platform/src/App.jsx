0aecce3dc6aca638fea7ba47e7cf706203939937
AdrianW26 <adrianwong299@gmail.com>
2025-03-18 01:31:06 +0000

    added signup page with tailwind

--- crypto-trading-platform/src/App.jsx
+++ crypto-trading-platform/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from "./Login"
+import SignUp from './SignUp'
 
 function App() {
 
 
   return (
     <>
-      <Login/>
+      <SignUp/>
     </>
   )
 }
