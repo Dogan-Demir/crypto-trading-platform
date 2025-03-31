d54b0cc697e3d052e7232bb118ac245dc1882171
Muneeb Musharaf <muneebmusharaf@gmail.com>
2025-03-31 19:00:27 +0100

    Extra changes to urls.py

--- crypto-trading-platform/mybackend/API_logic/urls.py
+++ crypto-trading-platform/mybackend/API_logic/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import getCoinPairs, getCoinPrice, BuyCrypto, SellCrypto, TradeHistory
 
 urlpatterns = [
-    path('pairs', getCoinPairs.as_view(), name = 'get-coin-pairs'),
-    path('price/<str:coin>/<str:quote_currency>', getCoinPrice.as_view(), name = 'get-coin-price'),
-    path('buy', BuyCrypto.as_view(), name = 'buy-crypto'),
-    path('sell', SellCrypto.as_view(), name = 'sell-crypto'),
+    path('pairs/', getCoinPairs.as_view(), name = 'get-coin-pairs'),
+    path('price/<str:coin>/<str:quote_currency>/', getCoinPrice.as_view(), name = 'get-coin-price'),
+    path('buy/', BuyCrypto.as_view(), name = 'buy-crypto'),
+    path('sell/', SellCrypto.as_view(), name = 'sell-crypto'),
     path('history/', TradeHistory.as_view(), name = 'trade-history'),
 ]
